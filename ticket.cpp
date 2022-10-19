8b97788ecabbc0efbc319ba31f6032ba966083a3
Pepengu <plotnikovdanya@gmail.com>
2022-10-19 21:57:00 +0300

    #include <iostream>
    int main(){
        int n;
        std::cin >> n;
        std::cout << ((n%10+n/10%10+n/100%10)/(n/1000%10+n/10000%10+n/100000%10)==1 ? "Lucky" : "Usual");
        return 0;
    }

---
+++ ticket.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::cout << ((n%10+n/10%10+n/100%10)/(n/1000%10+n/10000%10+n/100000%10)==1 ? "Lucky" : "Usual");
+    return 0;
+}
